40da5666e7aff2bce1fd2b7d159c86f81c0e4162
Shrey1528 <103505972+Shrey1528@users.noreply.github.com>
2022-12-17 16:55:32 +0530

    Initial Commit

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
+const Customers = () => {
+  const selectionsettings = { persistSelection: true };
+  const toolbarOptions = ["Delete"];
+  const editing = { allowDeleting: true, allowEditing: true };
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl ">
+      <Header category="page" title="Customers" />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        selectionSettings={selectionsettings}
+        toolbar={toolbarOptions}
+        editSettings={editing}
+        enableHover={false}
+        pageSettings={{ pageCount: 5 }}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Customers;
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        14: "14px",
+      },
+      backgroundColor: {
+        "main-bg": "#FAFBFB",
+        "main-dark-bg": "#20232A",
+        "secondary-dark-bg": "#33373E",
+        "light-gray": "#F7F7F7",
+        "half-transparent": "rgba(0, 0, 0, 0.5)",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      borderColor: {
+        color: "rgba(0, 0, 0, 0.1)",
+      },
+      width: {
+        400: "400px",
+        760: "760px",
+        780: "780px",
+        800: "800px",
+        1000: "1000px",
+        1200: "1200px",
+        1400: "1400px",
+      },
+      height: {
+        80: "80px",
+      },
+      minHeight: {
+        590: "590px",
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "url('https://cdn.discordapp.com/attachments/1049340355172126780/1051856808022638592/Pngtreecall_center_woman_with_headphones_7264160.png')",
+      },
+    },
+  },
+  plugins: [],
+};
